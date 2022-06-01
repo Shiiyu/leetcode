d054c7d3582df8f4695eebcc35d8d9b8f984b28f
Shiiyu <faithiikittii@gmail.com>
2022-06-01 07:26:41 -0400

    refactor: :fire: make tests more clean

--- rust/solutions/s0001_two_sum.rs
+++ rust/solutions/s0001_two_sum.rs
@@ -14,13 +14,8 @@ impl Solution {
   }
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn test() {
-    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
-    assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
-  }
+#[test]
+fn test() {
+  assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+  assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
 }
--- rust/solutions/s0009_palindrome_number.rs
+++ rust/solutions/s0009_palindrome_number.rs
@@ -8,14 +8,9 @@ impl Solution {
   }
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn test() {
-    assert_eq!(Solution::is_palindrome(121), true);
-    assert_eq!(Solution::is_palindrome(-121), false);
-    assert_eq!(Solution::is_palindrome(10), false);
-  }
+#[test]
+fn test() {
+  assert_eq!(Solution::is_palindrome(121), true);
+  assert_eq!(Solution::is_palindrome(-121), false);
+  assert_eq!(Solution::is_palindrome(10), false);
 }
