3ef7a7a74f05baa2d2cc053b90b876f40507d0c2
Korase <faithiikittii@gmail.com>
2022-06-02 10:14:43 -0400

    feat: :sparkles: s0028

---
+++ rust/solutions/s0028_implement_strstr.rs
@@ -0,0 +1,16 @@
+struct Solution;
+
+impl Solution {
+  pub fn str_str(haystack: String, needle: String) -> i32 {
+    match haystack.find(&needle) {
+      Some(i) => i as i32,
+      None => -1
+    }
+  }
+}
+
+#[test]
+fn test() {
+  assert_eq!(Solution::str_str("hello".to_string(), "ll".to_string()), 2);
+  assert_eq!(Solution::str_str("aaaaa".to_string(), "bba".to_string()), -1);
+}
