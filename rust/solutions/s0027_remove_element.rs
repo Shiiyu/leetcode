327d84a12b0144b0f138d862b87e2aa1a6b83562
Korase <faithiikittii@gmail.com>
2022-06-02 09:57:08 -0400

    feat: :sparkles: s0027

---
+++ rust/solutions/s0027_remove_element.rs
@@ -0,0 +1,14 @@
+struct Solution;
+
+impl Solution {
+  pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    nums.retain(|&n| n != val);
+    nums.len() as i32
+  }
+}
+
+#[test]
+fn test() {
+  assert_eq!(Solution::remove_element(&mut vec![3, 2, 2, 3], 3), 2);
+  assert_eq!(Solution::remove_element(&mut vec![0, 1, 2, 2, 3, 0, 4, 2], 2), 5);
+}
